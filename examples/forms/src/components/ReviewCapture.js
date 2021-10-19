6ada076dff099264d3cb2a4dfdc102f7f4f258e3
Soubhratra <soubhratra@instnt.org>
2021-10-19 11:08:41 -0400

    IN-1588 cosmetic UI update

--- examples/forms/src/components/ReviewCapture.js
+++ examples/forms/src/components/ReviewCapture.js
@@ -9,18 +9,29 @@ const ReviewCapture = (props) => {
 
   return (
     <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography variant="h5" component="div" gutterBottom>
+          Review Captured Image
+        </Typography>
+        </CardContent>
       <CardMedia
         component="img"
-        height="194"
+        height="300"
         image={props.captureResult?.result}
         alt="DL front"
       />
       <CardContent>
+        
         <Typography variant="body2" color="text.secondary">
           Review your picture and proceed
           Full document image is visible
           Text in the document is clear and readable
           No glare or shadow
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography sx={{ mb: 4.5 }} variant="body2" color="text.secondary">
+          To retake the picture click Back, to proceed click Next
         </Typography>
       </CardContent>
     </Card>
