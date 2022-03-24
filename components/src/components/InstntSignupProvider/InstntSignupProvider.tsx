385eb33b47a74791c248bc34ebbb47e054bc3b8f
Roman Zhukov <roman@instnt.org>
2022-03-24 19:42:55 -0400

    Asynchronous events

--- components/src/components/InstntSignupProvider/InstntSignupProvider.tsx
+++ components/src/components/InstntSignupProvider/InstntSignupProvider.tsx
@@ -2,61 +2,90 @@ import React, { useState, useEffect } from 'react';
 import InnerHTML from 'dangerously-set-html-content';
 import PropTypes from 'prop-types';
 
+const LIVE_SERVICE_URL = 'https://api.instnt.org';
+const SANDBOX_SERVICE_URL = 'https://sandbox-api.instnt.org';
 
 const propTypes = {
   formKey: PropTypes.string.isRequired,
-  serviceURL: PropTypes.string.isRequired,
-  children: PropTypes.any.isRequired
+  isAsync: PropTypes.bool,
+  onEvent: PropTypes.func,
+  sandbox: PropTypes.bool,
+  serviceURL: PropTypes.string,
+  children: PropTypes.node,
 };
 
 interface InstntSignupProviderProps {
   formKey: String;
-  onEvent: any;
-  sandbox: any;
-  serviceURL: any;
-  children: any;
+  isAsync?: Boolean;
+  onEvent?: Function;
+  sandbox?: Boolean;
+  serviceURL?: String;
+  children?: React.ReactNode;
 }
 
-const InstntSignupProvider = (props: InstntSignupProviderProps) => {
+const InstntSignupProvider = ({
+  formKey,
+  isAsync = false,
+  onEvent,
+  sandbox = false,
+  serviceURL = LIVE_SERVICE_URL,
+  children,
+}: InstntSignupProviderProps) => {
   const [instntFormCode, setInstntFormCode] = useState('');
 
   useEffect(() => {
-    (window as any).onInstntEvent = props.onEvent;
+    (window as any).instntSettings = {
+      isAsync: isAsync,
+      onEvent: onEvent,
+    };
+
+    (window as any).onInstntEvent = onEvent; // Deprecated
     (async () => {
-      const context = "initiating Instnt transaction";
-      const url = props.serviceURL + '/public/transactions?idmetrics_version=4.5.4';
+      const context = 'initiating Instnt transaction';
+      const url =
+        (sandbox ? SANDBOX_SERVICE_URL : serviceURL) +
+        '/public/transactions?idmetrics_version=4.5.4';
       try {
-        const response = await fetch(
-          url,
-          {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              form_key: props.formKey,
-              hide_form_fields: true,
-              redirect: false,
-            }),
-          }
-        );
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            form_key: formKey,
+            hide_form_fields: true,
+            redirect: false,
+          }),
+        });
         const data = await response.json();
         if (response.ok) {
           setInstntFormCode(data.html);
         } else {
-          console.log("Error processing " + url, data);
-          props.onEvent({
-            type: 'transaction.error',
-            data: { 'message': 'Received error: ' + data.errorMessage + ' while ' + context, 'status': data.status, 'type': 'error' }
-          });
+          console.log('Error processing ' + url, data);
+          if (onEvent) {
+            onEvent({
+              type: 'transaction.error',
+              data: {
+                message:
+                  'Received error: ' + data.errorMessage + ' while ' + context,
+                status: data.status,
+                type: 'error',
+              },
+            });
+          }
         }
       } catch (error) {
-        console.log("Error while connecting to " + url, error);
-        props.onEvent({
-          type: 'transaction.error',
-          data: { 'message': 'Received error: ' + error.message + ' while ' + context, 'type': 'error' }
-        });
+        console.log('Error while connecting to ' + url, error);
+        if (onEvent) {
+          onEvent({
+            type: 'transaction.error',
+            data: {
+              message: 'Received error: ' + error.message + ' while ' + context,
+              type: 'error',
+            },
+          });
+        }
       }
     })();
     return () => {
@@ -69,7 +98,7 @@ const InstntSignupProvider = (props: InstntSignupProviderProps) => {
       {instntFormCode && (
         <InnerHTML id="instnt-form-generator" html={instntFormCode} />
       )}
-      {props.children}
+      {children}
     </React.Fragment>
   );
 };
