961719ec7e69c9f4bf067daa3a4bb2fb3b52ee03
Vinay Vuriti <vuriti@instnt.org>
2022-10-27 22:16:35 +0530

    In-2955 stageversion.sh added and buildspec updated

---
+++ components/stageversion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm install
+npm run build
+echo "Build completed on `date`"
+pwd
+
+cat src/version.ts
+
+cd ..
+pwd
+
+a=$(awk 'NR==8 {print  }'  examples/forms/package.json)
+echo ${a}
+b=$(awk 'NR==8 {print $1 }'  examples/forms/package.json)
+echo ${b}
+c=$(awk 'NR==3 {print $NF}'  components/package.json)
+echo ${c}
+
+sed -i "s+${a}+    ${b} ${c}+" examples/forms/package.json
+
+
+cat examples/forms/package.json
