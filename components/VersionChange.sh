3b6ce5a87c6bcbab5681381a68bb7d65995845a9
Vinay Vuriti <vuriti@instnt.org>
2022-12-06 21:48:48 +0530

    IN-3043 Added CFT and updated buildspec and shell script

--- components/VersionChange.sh
+++ components/VersionChange.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+pwd
+
+if [[ ${Environment} == prod2 ]]; then
+    x=$(awk -F'[ .]' '/"version"/ {print $7*1}' package.json)
+    sed -i "s/-beta.${x}//g" package.json
+fi
+
 npm install
 npm run build
 echo "Build completed on `date`"
 pwd
-echo "Publishing to NPM"
-
-npm publish --tag beta
-echo "successfully published" 
-
 
 cat src/version.ts
 
+if [[ ${Environment} == dev2 ]]; then
+    echo "Publishing to NPM with beta tag"
+    npm publish --tag beta
+    echo "successfully published"
+fi
+
+if [[ ${Environment} == prod2 ]]; then
+    echo "Publishing to NPM"
+    npm publish
+    echo "successfully published"
+fi
+
+pwd
 cd ..
 pwd
 
@@ -23,6 +38,5 @@ c=$(awk 'NR==3 {print $NF}'  components/package.json)
 echo ${c}
 
 sed -i "s+${a}+    ${b} ${c}+" examples/forms/package.json
-
 
 cat examples/forms/package.json
