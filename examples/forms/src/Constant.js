53de897cc90886ee2cfdc283f1c1fefde4a48256
Pankaj Singh <pankaj@instnt.org>
2023-12-19 13:30:16 +0530

    Unit Test case for InstntSignupProvider Component

---
+++ examples/forms/src/Constant.js
@@ -0,0 +1,26 @@
+//export const FORM_KEY = 'v1697610991925022';// SSI Enable Workflow
+
+// export const formDataObj = [
+//     {
+//         FORM_KEY: 'v1658926416633893',
+//         SERVICE_URL: 'https://dev2-api.instnt.org',
+//         ID_METRIC_VERSION: '4.7.0',
+//         TRANSACTION_INITIATED_EVENT: "transaction.initiated",
+//         OTP_SENT_EVENT: "otp.sent",
+//         DOCUMENT_CAPTURE_FRAMEWORKLOADED: "document.capture-frameworkLoaded",
+//         TRANSACTION_PROCESSED: "transaction.processed",
+//         TRANSACTION_ERROR: "transaction.error"
+//     }
+// ]
+
+
+
+
+export const FORM_KEY = 'v1658926416633893';
+export const SERVICE_URL = 'https://dev2-api.instnt.org';
+export const ID_METRIC_VERSION = '4.7.0';
+export const TRANSACTION_INITIATED_EVENT = "transaction.initiated";
+export const OTP_SENT_EVENT = "otp.sent";
+export const DOCUMENT_CAPTURE_FRAMEWORKLOADED = "document.capture-frameworkLoaded";
+export const TRANSACTION_PROCESSED = "transaction.processed";
+export const TRANSACTION_ERROR = "transaction.error";
--- examples/forms/src/index.js
+++ examples/forms/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DocumentUploaderApp from './DocumentUploaderApp';
+import DocumentUploaderApp from './DocumentUploaderApp.js';
+import reportWebVitals from './reportWebVitals.js'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
